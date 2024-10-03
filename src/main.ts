7bb8994e73813405ff5c5b26afe4be2379db1889
Guilherme Filipi Sperb <guilherme.spe@gmail.com>
2024-10-03 19:01:45 -0300

    inserção de rota

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { ItemController } from './application/controller/item-controller';
 import { ItemRepositoryMemory } from './infra/repository/memory/item-repository-memory';
 import { TipoItemRepositoryMemory } from './infra/repository/memory/tipoitem-repository-memory';
 
+import { TipoItemController } from './application/controller/tipo-item-controller';
+import { PersonController } from './application/controller/person-controller';
+import { PessoaRepositoryMemory } from './infra/repository/memory/pessoa-repository-memory';
+
+
+
+
+
+
 const app = express();
 const port = 3004;
 
@@ -14,16 +23,38 @@ const tipoItemRepositoryMemory = new TipoItemRepositoryMemory()
 const itemRepositoryMemory = new ItemRepositoryMemory()
 const itemController = new ItemController(itemRepositoryMemory, tipoItemRepositoryMemory)
 
+const tipoItemcontroller = new TipoItemController(tipoItemRepositoryMemory)
+const pessoaRepositoryMemory = new PessoaRepositoryMemory()
+const personController = new PersonController(pessoaRepositoryMemory)
+
+
 app.get('/itens', (request, response)=> {
    response.send( itemController.getAll({}));
 })
 
 app.post('/itens', (request, response) => {
     response.send(itemController.create(request.body));
-}
+})
 
 
-)
+
+app.post('/tipoitem',(request, response) =>{
+    response.send(tipoItemcontroller.create(request.body))
+})
+
+app.get('/tipoitem',(request, response) => {
+    response.send(tipoItemcontroller.getAll({}))
+})
+
+
+app.get('/pessoas',(request, response)=> {
+    response.send(personController.getAll({}))
+});
+
+app.post('/pessoas', (request, response) => {
+    response.send(personController.create(request.body));
+
+})
 
 app.listen(port, () => {
     console.log("servidor iniciado na porta : "+ port)
