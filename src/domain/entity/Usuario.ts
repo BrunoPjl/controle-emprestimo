9a07e9fa8cd02d7197e092e0a426752f9c286928
BRUNO OTAVIO BISOLO <bruno_o_bisolo@estudante.sc.senai.br>
2024-11-29 22:01:06 -0300

    inserção de login

--- src/domain/entity/Usuario.ts
+++ src/domain/entity/Usuario.ts
@@ -40,4 +40,5 @@ getSenha(): string{
 
 }
 
+
 //beleza sobe
--- src/domain/repository/repository-factory.ts
+++ src/domain/repository/repository-factory.ts
@@ -1,7 +1,9 @@
 import { ItemRepository } from "./item-repository";
 import { TipoItemRepository } from "./tipo-item-repository";
+import { UsuarioRepository } from "./usuario-repository";
 
 export interface RepositoryFactory {
     createItemRepository(): ItemRepository
     createTipoItemRepository(): TipoItemRepository;
+    createUserRepository(): UsuarioRepository;
 }
